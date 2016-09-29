ab34ba12bbbc75860a44739dd210222a3c7895bb
Dehua Cheng <chengdehua7197@gmail.com>
2016-09-29 11:53:32 -0700

    add Recording; fix Factor Normalizatio bug

--- include/TensorAls.h
+++ include/TensorAls.h
@@ -7,8 +7,11 @@ class TensorCP_ALS;
 #include "TensorData.h"
 
 #include <vector>
+#include <chrono>
 #include <omp.h>
 #include <memory>
+
+using namespace std;
 
 class TensorCP_ALS
 {
@@ -16,19 +19,30 @@ class TensorCP_ALS
     TensorCP_ALS(const TensorData &_data, shared_ptr<CPDecomp> &_cpd);
 
     void randInit(RNGeng *rng);
-
     virtual int updateFactor(const unsigned factorId);
 
+    void setErrorRecordInterval(const size_t _interval);
+    void setVerbose(const int _verbose);
     const size_t rank;
 
   protected:
     const TensorData &data;
     shared_ptr<CPDecomp> &cpd;
     size_t currIter;
+    int verbose;
+
+    size_t RecordError;
+    vector<pair<size_t, T>> absErrorIter;
+    vector<pair<size_t, T>> rltErrorIter;
+    vector<double> secIter;
+    vector<size_t> factorIter;
 
     // pre allocated memory
     vector<vector<vector<T>>> gramABpdt;
     vector<vector<vector<T>>> gramABpdtInv;
+
+    void logAfterIter(const unsigned factorId, chrono::time_point<chrono::system_clock> &stepStartTime);
+    chrono::time_point<chrono::system_clock> logBeforIter(const unsigned factorId);
 
     void prepareGramInv(const size_t factorId);
     void updateEntry(const unsigned factorId, const vector<size_t> &froms, const size_t i, const vector<vector<T>> &gramABInv, const T weight);
--- src/TensorAls.cpp
+++ src/TensorAls.cpp
@@ -2,6 +2,7 @@
 #include "TensorAls.h"
 #include "CPDecomp.h"
 #include "TensorData.h"
+#include "SpAlsLinalg.h"
 
 #include <memory>
 #include <vector>
@@ -15,40 +16,83 @@ TensorCP_ALS::TensorCP_ALS(const TensorData &_data, shared_ptr<CPDecomp> &_cpd) 
     gramABpdtInv = vector<vector<vector<T>>>(data.dims.size(), vector<vector<T>>(rank, vector<T>(rank)));
 }
 
+chrono::time_point<chrono::system_clock> TensorCP_ALS::logBeforIter(const unsigned factorId)
+{
+    factorIter.push_back(factorId);
+
+    return chrono::system_clock::now();
+}
+
+void TensorCP_ALS::setErrorRecordInterval(const size_t _interval)
+{
+    RecordError = _interval;
+}
+
+void TensorCP_ALS::setVerbose(const int _verbose)
+{
+    verbose = _verbose;
+}
+
+void TensorCP_ALS::logAfterIter(const unsigned factorId, chrono::time_point<chrono::system_clock> &stepStartTime)
+{
+    // log after iteration
+    if (RecordError)
+    {
+        if (currIter % RecordError == 0)
+        {
+            absErrorIter.push_back(pair<size_t, T>(currIter, Linalg::Fnorm2Diff(data, *cpd)));
+            rltErrorIter.push_back(pair<size_t, T>(currIter, absErrorIter.back().second / data.normData()));
+            if (verbose)
+            {
+                cout << "Iteration:\t" << currIter << "\t Factor:" << factorId << "\n\tAbs. Err.:"
+                     << absErrorIter.back().second << "\tRel. Err.:" << rltErrorIter.back().second << endl;
+            }
+        }
+    }
+    //record time
+    auto stepEndTime = chrono::system_clock::now();
+    chrono::duration<double> stepTime(0);
+    stepTime += stepEndTime - stepStartTime;
+    secIter.push_back(stepTime.count());
+}
+
 int TensorCP_ALS::updateFactor(const unsigned factorId)
 {
+    // log before iteration
+    auto stepStartTime = logBeforIter(factorId);
 
-    chrono::duration<double> stepTime(0);
-    auto stepStartTime = chrono::system_clock::now();
-
-    currIter++;
+    // clear the
     cpd->lambdas = vector<T>(rank, 1.0);
     SpAlsUtils::reset(cpd->factors[factorId]);
+    cpd->isFactorNormalized[factorId] = false;
 
+    //get inverse gram matrix
     prepareGramInv(factorId);
-    vector<size_t> froms = SpAlsUtils::getFroms(factorId, cpd->dims.size());
 
+    vector<size_t> froms = SpAlsUtils::getFroms(factorId, cpd->dims.size());
     for (size_t i = 0; i < data.nnz; i++)
     {
         updateEntry(factorId, froms, i, gramABpdtInv[factorId], 1.0);
     }
 
+    //postprocessing of the new factor
+    if (verbose > 2)
+    {
+        cout << "print Factor Matrix Before Normalization:\n";
+        SpAlsUtils::printMatrix(cpd->ro_factors[factorId]);
+    }
+
     cpd->normalizeFactor(factorId);
+    if (verbose > 2)
+    {
+        cout << "print Factor Matrix After Normalization:\n";
+        SpAlsUtils::printMatrix(cpd->ro_factors[factorId]);
+    }
+
     cpd->getGramMtx(factorId);
 
-    // if (!RecordError)
-    // {
-    //     if (currIter % RecordError == 0)
-    //     {
-    //         absErrorIter.push_back(diffOnly());
-    //         rltErrorIter.push_back(absErrorIter.back() / normT);
-    //     }
-    // }
-    // //record time
-    auto stepEndTime = chrono::system_clock::now();
-    stepTime += stepEndTime - stepStartTime;
-    // secIter.push_back(stepTime.count());
-
+    logAfterIter(factorId, stepStartTime);
+    currIter++;
     return 0;
 }
 
@@ -76,6 +120,13 @@ void TensorCP_ALS::prepareGramInv(const size_t factorId)
         }
     }
     SpAlsUtils::invert(gramAB, gramABInv, rank);
+    if (verbose > 2)
+    {
+        cout << "print Gram Matrix:\n";
+        SpAlsUtils::printMatrix(gramAB);
+        cout << "print Inverse Gram Matrix:\n";
+        SpAlsUtils::printMatrix(gramABInv);
+    }
 }
 
 void TensorCP_ALS::updateEntry(const unsigned factorId, const vector<size_t> &froms,
